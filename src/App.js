344caa44c4c0ec3623981680de3d3c172d8c4473
Abdulazeez Abdulsobur <77147508+Dammy46@users.noreply.github.com>
2022-05-28 11:02:37 +0100

    Typos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar'
-import Hero from './components/Hero_section/hero';
+import Hero from './components/hero_section/hero';
 import Section from './components/Section/section'
 import Subscribe from './components/Subscribtion/sub'
 // import { useState } from 'react';
