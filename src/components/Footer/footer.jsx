a7f33c16880a1c2386c7813ac77a5da63f669784
Abdulazeez Abdulsobur <77147508+Dammy46@users.noreply.github.com>
2022-05-28 21:50:29 +0100

    Href value

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,7 +4,7 @@ const footer = () => {
   return (
     <div className="container mx-auto px-8 pt-24 lg:flex lg:justify-around md:flex md:justify-between block">
       <div className="w-72 lg:block hidden ">
-        <a href="#">
+        <a href="index">
           <img
             src="	https://the-design-buddy.netlify.app/assets/bottom.8038c058.svg"
             alt="logo"
@@ -15,46 +15,46 @@ const footer = () => {
       <ul className="mb-4">
         <h5 className="text-xl font-medium text-slate-900">Quick Link</h5>
         <li className="my-5 ">
-          <a href="#">About</a>
+          <a href="index">About</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Help Center</a>
+          <a href="index">Help Center</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Term & Policy</a>
+          <a href="index">Term & Policy</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Privacy Policy</a>
+          <a href="index">Privacy Policy</a>
         </li>
       </ul>
       <ul className="mb-4">
         <h5 className="text-xl font-medium text-slate-900">Services</h5>
         <li className="my-5 ">
-          <a href="#">Find Job</a>
+          <a href="index">Find Job</a>
         </li>
         <li className="my-5 ">
           <a href="#">Post a job</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Hire for your team</a>
+          <a href="index">Hire for your team</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Press</a>
+          <a href="index">Press</a>
         </li>
       </ul>
       <ul className="mb-4">
         <h5 className="text-xl font-medium text-slate-900">Quick Link</h5>
         <li className="my-5 ">
-          <a href="#">Twitter</a>
+          <a href="index">Twitter</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Linkedin</a>
+          <a href="index">Linkedin</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Facebook</a>
+          <a href="index">Facebook</a>
         </li>
         <li className="my-5 ">
-          <a href="#">Blog</a>
+          <a href="index">Blog</a>
         </li>
       </ul>
     </div>
