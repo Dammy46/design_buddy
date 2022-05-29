303b29ad617c2a7a0345d1bdcec4a10b379ed29c
Dev Dammy <cranedammy46@gmail.com>
2022-05-29 18:03:20 +0100

    update

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const footer = () => {
   return (
-    <div className="container mx-auto px-8 pt-24 lg:flex lg:justify-around md:flex md:justify-between block">
-      <div className="lg:w-72 lg:block w-40 lg:mb-0 mb-11 ">
+    <div className="container mx-auto px-8 pt-24 lg:flex lg:justify-around md:flex md:justify-between block ">
+      <div className="lg:w-72 lg:block  lg:mb-0 mb-11 ">
         <a href="index">
           <img
             src="	https://the-design-buddy.netlify.app/assets/bottom.8038c058.svg"
@@ -14,16 +14,16 @@ const footer = () => {
       </div>
       <ul className="mb-8">
         <h5 className="text-xl font-medium text-slate-900">Quick Link</h5>
-        <li className="my-5 text-base font-light">
+        <li className="my-4 text-base font-light">
           <a href="index">About</a>
         </li>
-        <li className="my-5 text-base font-light">
+        <li className="my-4 text-base font-light">
           <a href="index">Help Center</a>
         </li>
-        <li className="my-5 text-base font-light">
+        <li className="my-4 text-base font-light">
           <a href="index">Term & Policy</a>
         </li>
-        <li className="my-5 text-base font-light">
+        <li className="my-4 text-base font-light">
           <a href="index">Privacy Policy</a>
         </li>
       </ul>
