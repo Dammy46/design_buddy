157041f9a1bc3e11f2955b49473f2288b4c910c0
Dev Dammy <cranedammy46@gmail.com>
2022-06-04 21:20:03 +0100

    add some couple of routes

--- src/components/pagination/pagination.jsx
+++ src/components/pagination/pagination.jsx
@@ -9,13 +9,13 @@ const pagination = ({ postsPerPage, totaljob }) => {
     <div className="bg-white px-4 py-3 flex items-center justify-between sm:px-6">
       <div className="flex-1 flex justify-between sm:hidden">
         <a
-          href="#"
+          href="index"
           className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
         >
           Previous
         </a>
         <a
-          href="#"
+          href="index"
           className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
         >
           Next
@@ -28,7 +28,7 @@ const pagination = ({ postsPerPage, totaljob }) => {
             aria-label="Pagination"
           >
             <a
-              href="#"
+              href="index"
               className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
             >
               <span className="sr-only">Previous</span>
@@ -36,20 +36,20 @@ const pagination = ({ postsPerPage, totaljob }) => {
             </a>
 
             <a
-              href="#"
+              href="index"
               aria-current="page"
               className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
             >
               1
             </a>
             <a
-              href="#"
+              href="index"
               className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
             >
               2
             </a>
             <a
-              href="#"
+              href="index"
               className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
             >
               3
@@ -58,25 +58,25 @@ const pagination = ({ postsPerPage, totaljob }) => {
               ...
             </span>
             <a
-              href="#"
+              href="index"
               className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
             >
               8
             </a>
             <a
-              href="#"
+              href="index"
               className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
             >
               9
             </a>
             <a
-              href="#"
+              href="index"
               className="bg-white border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
             >
               10
             </a>
             <a
-              href="#"
+              href="index"
               className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
             >
               <span className="sr-only">Next</span>
